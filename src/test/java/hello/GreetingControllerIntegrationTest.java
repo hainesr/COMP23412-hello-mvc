947fbc749ec187969abb9f938bec2e6a30fd2fdd
Robert Haines <rhaines@manchester.ac.uk>
2016-12-19 15:50:29 +0000

    Add tests for the greeting controller.

---
+++ src/test/java/hello/GreetingControllerIntegrationTest.java
@@ -0,0 +1,43 @@
+package hello;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingControllerIntegrationTest {
+
+	@LocalServerPort
+	private int port;
+
+	private URL greeting;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		this.greeting = new URL("http://localhost:" + port + "/greeting");
+	}
+
+	@Test
+	public void getGreeting() {
+		ResponseEntity<String> response = template.getForEntity(greeting.toString(), String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+	}
+
+}
