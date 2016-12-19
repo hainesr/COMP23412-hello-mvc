ba47e6d9845093d643157a44a2bc01c7f816d4ed
Robert Haines <rhaines@manchester.ac.uk>
2016-12-19 15:44:41 +0000

    Add test for the root of the site.

---
+++ src/test/java/hello/RootControllerTest.java
@@ -0,0 +1,28 @@
+package hello;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RootControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getRoot() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML)).andExpect(status().isOk());
+	}
+
+}
