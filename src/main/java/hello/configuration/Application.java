93c4d30661080bf00f08db23a7b10dfaaf9e3c08
Robert Haines <rhaines@manchester.ac.uk>
2016-12-20 10:39:56 +0000

    Fix configuration of class scanning.

--- src/main/java/hello/configuration/Application.java
+++ src/main/java/hello/configuration/Application.java
@@ -2,10 +2,8 @@ package hello.configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "hello")
+@SpringBootApplication(scanBasePackages = "hello")
 public class Application {
 
 	public static void main(String[] args) {
