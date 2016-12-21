5f88d16a20891e4e6ea568b71019e50487e94512
Robert Haines <rhaines@manchester.ac.uk>
2016-12-21 10:59:19 +0000

    Test for HTML response in integration tests.

--- src/test/java/hello/controllers/GreetingControllerIntegrationTest.java
+++ src/test/java/hello/controllers/GreetingControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertThat;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +35,8 @@ public class GreetingControllerIntegrationTest {
 	private URL greeting;
 	private URL greetingName;
 
+	private HttpEntity<String> entity;
+
 	@Autowired
 	private TestRestTemplate template;
 
@@ -39,6 +45,18 @@ public class GreetingControllerIntegrationTest {
 		String url = "http://localhost:" + port + "/greeting";
 		this.greeting = new URL(url);
 		this.greetingName = new URL(url + "?name=Rob");
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.TEXT_HTML));
+		entity = new HttpEntity<String>("parameters", headers);
+	}
+
+	@Test
+	public void getHtmlGreeting() {
+		ResponseEntity<String> response = template.exchange(greeting.toString(), HttpMethod.GET, entity, String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		assertThat(response.getHeaders().getContentType().toString(), containsString(MediaType.TEXT_HTML_VALUE));
+		assertThat(response.getBody(), containsString("Hello, World!"));
 	}
 
 	@Test
@@ -47,6 +65,15 @@ public class GreetingControllerIntegrationTest {
 		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
 		assertThat(response.getHeaders().getContentType(), equalTo(MediaType.APPLICATION_JSON_UTF8));
 		assertThat(response.getBody(), containsString("Hello, World!"));
+	}
+
+	@Test
+	public void getHtmlGreetingName() {
+		ResponseEntity<String> response = template.exchange(greetingName.toString(), HttpMethod.GET, entity,
+				String.class);
+		assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+		assertThat(response.getHeaders().getContentType().toString(), containsString(MediaType.TEXT_HTML_VALUE));
+		assertThat(response.getBody(), containsString("Hello, Rob!"));
 	}
 
 	@Test
