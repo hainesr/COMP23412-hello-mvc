21e9e1e70837949ce44d12f291a5e40bbf3dae53
Robert Haines <rhaines@manchester.ac.uk>
2017-01-02 11:01:39 +0000

    Make new config package and move config into it.
    Only got content negotiation so far, but more will follow.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,22 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false).useJaf(false)
-		.defaultContentType(MediaType.TEXT_HTML).mediaType("html", MediaType.TEXT_HTML)
-		.mediaType("json", MediaType.APPLICATION_JSON);
 	}
 
 }
---
+++ src/main/java/hello/config/ContentNegotiation.java
@@ -0,0 +1,18 @@
+package hello.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ContentNegotiation extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false).useJaf(false)
+				.defaultContentType(MediaType.TEXT_HTML).mediaType("html", MediaType.TEXT_HTML)
+				.mediaType("json", MediaType.APPLICATION_JSON);
+	}
+
+}
