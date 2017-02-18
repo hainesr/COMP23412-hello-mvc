d154f16f4ede32de66a5c3ecd690fd2ff3896c7f
Robert Haines <rhaines@manchester.ac.uk>
2017-02-18 10:03:56 +0000

    Separate data config for default and test profiles.

--- src/main/java/hello/config/DataLayer.java
+++ src/main/java/hello/config/DataLayer.java
@@ -1,17 +1,10 @@
 package hello.config;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -19,22 +12,9 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-@Profile("default")
-public class DefaultDataLayer {
+public class DataLayer {
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	private final static String PACKAGES = "hello.models";
-
-	@Bean
-	public DataSource dataSource(DataSourceProperties dataSourceProperties) {
-		Path dbPath = Paths.get(System.getProperty("user.dir"), "db", "data");
-		String dbOpts = "DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE";
-
-		log.info("Setting H2 path: " + dbPath);
-		log.info("Setting H2 options: " + dbOpts);
-
-		return dataSourceProperties.initializeDataSourceBuilder().url("jdbc:h2:" + dbPath + ";" + dbOpts).build();
-	}
 
 	@Bean
 	public EntityManagerFactory entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
@@ -61,5 +41,4 @@ public class DefaultDataLayer {
 	public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate(DataSource dataSource) {
 		return new NamedParameterJdbcTemplate(dataSource);
 	}
-
 }
