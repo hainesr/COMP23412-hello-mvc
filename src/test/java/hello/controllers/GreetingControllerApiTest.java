7187c89f48d087e54f2fd327b9fdc2bc986eba22
Robert Haines <rhaines@manchester.ac.uk>
2021-01-22 18:19:48 +0000

    Tidy up and reformat the unit tests.

--- src/test/java/hello/controllers/GreetingControllerApiTest.java
+++ src/test/java/hello/controllers/GreetingControllerApiTest.java
@@ -9,6 +9,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -28,7 +30,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import hello.config.Security;
 import hello.dao.GreetingService;
@@ -52,24 +53,22 @@ public class GreetingControllerApiTest {
 
 	@Test
 	public void getEmptyGreetingsList() throws Exception {
-		when(greetingService.findAll()).thenReturn(Collections.<Greeting> emptyList());
+		when(greetingService.findAll()).thenReturn(Collections.<Greeting>emptyList());
 
-		mvc.perform(MockMvcRequestBuilders.get("/api/greeting").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk()).andExpect(handler().methodName("list"))
-		.andExpect(jsonPath("$.length()", equalTo(1)))
-		.andExpect(jsonPath("$._links.self.href", endsWith("/api/greeting")));
+		mvc.perform(get("/api/greeting").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("list")).andExpect(jsonPath("$.length()", equalTo(1)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/greeting")));
 	}
 
 	@Test
 	public void getGreetingsList() throws Exception {
 		Greeting g = new Greeting("%s");
-		when(greetingService.findAll()).thenReturn(Collections.<Greeting> singletonList(g));
+		when(greetingService.findAll()).thenReturn(Collections.<Greeting>singletonList(g));
 
-		mvc.perform(MockMvcRequestBuilders.get("/api/greeting").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk()).andExpect(handler().methodName("list"))
-		.andExpect(jsonPath("$.length()", equalTo(2)))
-		.andExpect(jsonPath("$._links.self.href", endsWith("/api/greeting")))
-		.andExpect(jsonPath("$._embedded.greetings.length()", equalTo(1)));
+		mvc.perform(get("/api/greeting").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(handler().methodName("list")).andExpect(jsonPath("$.length()", equalTo(2)))
+				.andExpect(jsonPath("$._links.self.href", endsWith("/api/greeting")))
+				.andExpect(jsonPath("$._embedded.greetings.length()", equalTo(1)));
 	}
 
 	@Test
@@ -78,42 +77,41 @@ public class GreetingControllerApiTest {
 		Greeting g = new Greeting("%s");
 		when(greetingService.findOne(id)).thenReturn(g);
 
-		mvc.perform(MockMvcRequestBuilders.get("/api/greeting/{id}", id).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-		.andExpect(handler().methodName("greeting")).andExpect(jsonPath("$.template", equalTo("%s")))
-		.andExpect(jsonPath("$._links.self.href", endsWith("" + id)));
+		mvc.perform(get("/api/greeting/{id}", id).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(handler().methodName("greeting")).andExpect(jsonPath("$.template", equalTo("%s")))
+				.andExpect(jsonPath("$._links.self.href", endsWith("" + id)));
 	}
 
 	@Test
 	public void getNewGreeting() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/api/greeting/new").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isNotAcceptable()).andExpect(handler().methodName("newGreeting"));
+		mvc.perform(get("/api/greeting/new").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotAcceptable())
+				.andExpect(handler().methodName("newGreeting"));
 	}
 
 	@Test
 	public void postGreetingNoAuth() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/api/greeting").contentType(MediaType.APPLICATION_JSON)
+		mvc.perform(post("/api/greeting").contentType(MediaType.APPLICATION_JSON)
 				.content("{ \"template\": \"Howdy, %s!\" }").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isUnauthorized());
+				.andExpect(status().isUnauthorized());
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postGreetingBadAuth() throws Exception {
-		mvc.perform(
-				MockMvcRequestBuilders.post("/api/greeting").with(anonymous())
-				.contentType(MediaType.APPLICATION_JSON).content("{ \"template\": \"Howdy, %s!\" }")
-				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized());
+		mvc.perform(post("/api/greeting").with(anonymous()).contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"template\": \"Howdy, %s!\" }").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isUnauthorized());
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postGreetingBadRole() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/api/greeting").with(user("Rob").roles(BAD_ROLE))
-				.contentType(MediaType.APPLICATION_JSON).content("{ \"template\": \"Howdy, %s!\" }")
-				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+		mvc.perform(post("/api/greeting").with(user("Rob").roles(BAD_ROLE)).contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"template\": \"Howdy, %s!\" }").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden());
 
 		verify(greetingService, never()).save(greeting);
 	}
@@ -122,13 +120,11 @@ public class GreetingControllerApiTest {
 	public void postGreeting() throws Exception {
 		ArgumentCaptor<Greeting> arg = ArgumentCaptor.forClass(Greeting.class);
 
-		mvc.perform(
-				MockMvcRequestBuilders.post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ \"template\": \"Howdy, %s!\" }").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isCreated()).andExpect(content().string(""))
-		.andExpect(header().string("Location", containsString("/api/greeting/")))
-		.andExpect(handler().methodName("createGreeting"));
+		mvc.perform(post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON).content("{ \"template\": \"Howdy, %s!\" }")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated()).andExpect(content().string(""))
+				.andExpect(header().string("Location", containsString("/api/greeting/")))
+				.andExpect(handler().methodName("createGreeting"));
 
 		verify(greetingService).save(arg.capture());
 		assertThat("Howdy, %s!", equalTo(arg.getValue().getTemplate()));
@@ -136,36 +132,31 @@ public class GreetingControllerApiTest {
 
 	@Test
 	public void postBadGreeting() throws Exception {
-		mvc.perform(
-				MockMvcRequestBuilders.post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ \"template\": \"no placeholder\" }").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
-		.andExpect(handler().methodName("createGreeting"));
+		mvc.perform(post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON).content("{ \"template\": \"no placeholder\" }")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnprocessableEntity())
+				.andExpect(content().string("")).andExpect(handler().methodName("createGreeting"));
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postLongGreeting() throws Exception {
-		mvc.perform(
-				MockMvcRequestBuilders.post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+		mvc.perform(post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{ \"template\": \"abcdefghij %s klmnopqrst uvwxyz\" }").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
-		.andExpect(handler().methodName("createGreeting"));
+				.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
+				.andExpect(handler().methodName("createGreeting"));
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postEmptyGreeting() throws Exception {
-		mvc.perform(
-				MockMvcRequestBuilders.post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_JSON)
-				.content("{ \"template\": \"\" }").accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isUnprocessableEntity()).andExpect(content().string(""))
-		.andExpect(handler().methodName("createGreeting"));
+		mvc.perform(post("/api/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_JSON).content("{ \"template\": \"\" }")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnprocessableEntity())
+				.andExpect(content().string("")).andExpect(handler().methodName("createGreeting"));
 
 		verify(greetingService, never()).save(greeting);
 	}
--- src/test/java/hello/controllers/GreetingControllerTest.java
+++ src/test/java/hello/controllers/GreetingControllerTest.java
@@ -10,6 +10,8 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.flash;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
@@ -31,7 +33,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import hello.config.Security;
 import hello.dao.GreetingService;
@@ -55,20 +56,20 @@ public class GreetingControllerTest {
 
 	@Test
 	public void getEmptyGreetingsList() throws Exception {
-		when(greetingService.findAll()).thenReturn(Collections.<Greeting> emptyList());
+		when(greetingService.findAll()).thenReturn(Collections.<Greeting>emptyList());
 
-		mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
-		.andExpect(view().name("greeting/index")).andExpect(handler().methodName("list"));
+		mvc.perform(get("/greeting").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("greeting/index")).andExpect(handler().methodName("list"));
 
 		verifyZeroInteractions(greeting);
 	}
 
 	@Test
 	public void getGreetingsList() throws Exception {
-		when(greetingService.findAll()).thenReturn(Collections.<Greeting> singletonList(greeting));
+		when(greetingService.findAll()).thenReturn(Collections.<Greeting>singletonList(greeting));
 
-		mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
-		.andExpect(view().name("greeting/index")).andExpect(handler().methodName("list"));
+		mvc.perform(get("/greeting").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("greeting/index")).andExpect(handler().methodName("list"));
 
 		verify(greeting, atLeastOnce()).getId();
 		verify(greeting).getTemplate();
@@ -78,8 +79,8 @@ public class GreetingControllerTest {
 	public void getGreeting() throws Exception {
 		when(greetingService.findOne(1)).thenReturn(greeting);
 
-		mvc.perform(MockMvcRequestBuilders.get("/greeting/1").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
-		.andExpect(view().name("greeting/show")).andExpect(handler().methodName("greeting"));
+		mvc.perform(get("/greeting/1").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("greeting/show")).andExpect(handler().methodName("greeting"));
 
 		verify(greeting).getGreeting("World");
 	}
@@ -88,48 +89,47 @@ public class GreetingControllerTest {
 	public void getGreetingName() throws Exception {
 		when(greetingService.findOne(1)).thenReturn(greeting);
 
-		mvc.perform(MockMvcRequestBuilders.get("/greeting/1?name=Rob").accept(MediaType.TEXT_HTML))
-		.andExpect(status().isOk()).andExpect(view().name("greeting/show"))
-		.andExpect(handler().methodName("greeting"));
+		mvc.perform(get("/greeting/1?name=Rob").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+				.andExpect(view().name("greeting/show")).andExpect(handler().methodName("greeting"));
 
 		verify(greeting).getGreeting("Rob");
 	}
 
 	@Test
 	public void getNewGreetingNoAuth() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/greeting/new").accept(MediaType.TEXT_HTML))
-		.andExpect(status().isFound()).andExpect(header().string("Location", endsWith("/sign-in")));
+		mvc.perform(get("/greeting/new").accept(MediaType.TEXT_HTML)).andExpect(status().isFound())
+				.andExpect(header().string("Location", endsWith("/sign-in")));
 	}
 
 	@Test
 	public void getNewGreeting() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/greeting/new").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.accept(MediaType.TEXT_HTML))
-		.andExpect(status().isOk()).andExpect(view().name("greeting/new"))
-		.andExpect(handler().methodName("newGreeting"));
+		mvc.perform(get("/greeting/new").with(user("Rob").roles(Security.ADMIN_ROLE)).accept(MediaType.TEXT_HTML))
+				.andExpect(status().isOk()).andExpect(view().name("greeting/new"))
+				.andExpect(handler().methodName("newGreeting"));
 	}
 
 	@Test
 	public void postGreetingNoAuth() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.param("template", "Howdy, %s!").accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
-		.andExpect(header().string("Location", endsWith("/sign-in")));
+		mvc.perform(post("/greeting").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "Howdy, %s!")
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound())
+				.andExpect(header().string("Location", endsWith("/sign-in")));
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postGreetingBadRole() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(BAD_ROLE))
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "Howdy, %s!")
-				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isForbidden());
+		mvc.perform(
+				post("/greeting").with(user("Rob").roles(BAD_ROLE)).contentType(MediaType.APPLICATION_FORM_URLENCODED)
+						.param("template", "Howdy, %s!").accept(MediaType.TEXT_HTML).with(csrf()))
+				.andExpect(status().isForbidden());
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postGreetingNoCsrf() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+		mvc.perform(post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
 				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "Howdy, %s!")
 				.accept(MediaType.TEXT_HTML)).andExpect(status().isForbidden());
 
@@ -140,12 +140,11 @@ public class GreetingControllerTest {
 	public void postGreeting() throws Exception {
 		ArgumentCaptor<Greeting> arg = ArgumentCaptor.forClass(Greeting.class);
 
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.param("template", "Howdy, %s!").accept(MediaType.TEXT_HTML).with(csrf()))
-		.andExpect(status().isFound()).andExpect(content().string(""))
-		.andExpect(view().name("redirect:/greeting")).andExpect(model().hasNoErrors())
-		.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeExists("ok_message"));
+		mvc.perform(post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "Howdy, %s!")
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isFound()).andExpect(content().string(""))
+				.andExpect(view().name("redirect:/greeting")).andExpect(model().hasNoErrors())
+				.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeExists("ok_message"));
 
 		verify(greetingService).save(arg.capture());
 		assertThat("Howdy, %s!", equalTo(arg.getValue().getTemplate()));
@@ -153,37 +152,35 @@ public class GreetingControllerTest {
 
 	@Test
 	public void postBadGreeting() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.param("template", "no placeholder").accept(MediaType.TEXT_HTML).with(csrf()))
-		.andExpect(status().isOk())
-		.andExpect(view().name("greeting/new"))
-		.andExpect(model().attributeHasFieldErrors("greeting", "template"))
-		.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
+		mvc.perform(post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "no placeholder")
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isOk())
+				.andExpect(view().name("greeting/new"))
+				.andExpect(model().attributeHasFieldErrors("greeting", "template"))
+				.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postLongGreeting() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.param("template", "abcdefghij %s klmnopqrst uvwxyz").accept(MediaType.TEXT_HTML).with(csrf()))
-		.andExpect(status().isOk()).andExpect(view().name("greeting/new"))
-		.andExpect(model().attributeHasFieldErrors("greeting", "template"))
-		.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
+		mvc.perform(post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "abcdefghij %s klmnopqrst uvwxyz")
+				.accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isOk())
+				.andExpect(view().name("greeting/new"))
+				.andExpect(model().attributeHasFieldErrors("greeting", "template"))
+				.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
 
 		verify(greetingService, never()).save(greeting);
 	}
 
 	@Test
 	public void postEmptyGreeting() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.param("template", "").accept(MediaType.TEXT_HTML).with(csrf())).andExpect(status().isOk())
-		.andExpect(view().name("greeting/new"))
-		.andExpect(model().attributeHasFieldErrors("greeting", "template"))
-		.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
+		mvc.perform(post("/greeting").with(user("Rob").roles(Security.ADMIN_ROLE))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED).param("template", "").accept(MediaType.TEXT_HTML)
+				.with(csrf())).andExpect(status().isOk()).andExpect(view().name("greeting/new"))
+				.andExpect(model().attributeHasFieldErrors("greeting", "template"))
+				.andExpect(handler().methodName("createGreeting")).andExpect(flash().attributeCount(0));
 
 		verify(greetingService, never()).save(greeting);
 	}
