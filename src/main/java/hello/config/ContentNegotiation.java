b455668349a0854282f5fe0d1f35f839ea1e2f9b
Robert Haines <rhaines@manchester.ac.uk>
2022-01-03 19:15:53 +0000

    Remove deprecated content negotiation config.
    Now we're using Spring 5.3 underneath, the default behaviour is what we
    want and we no longer need the deprecated configuration method.

--- src/main/java/hello/config/ContentNegotiation.java
+++ src/main/java/hello/config/ContentNegotiation.java
@@ -8,13 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ContentNegotiation implements WebMvcConfigurer {
 
-	// Although deprecated we need to continue to use `favorPathExtension(false)`
-	// until Spring 5.3.
-	@SuppressWarnings("deprecation")
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorPathExtension(false).favorParameter(false).ignoreAcceptHeader(false)
-				.defaultContentType(MediaType.TEXT_HTML).mediaType("html", MediaType.TEXT_HTML)
-				.mediaType("json", MediaType.APPLICATION_JSON);
+		configurer.favorParameter(false).ignoreAcceptHeader(false).defaultContentType(MediaType.TEXT_HTML)
+				.mediaType("html", MediaType.TEXT_HTML).mediaType("json", MediaType.APPLICATION_JSON);
 	}
 }
