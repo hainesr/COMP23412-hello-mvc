1e4e88a729acccdb68f783a6b87306e7c4128ba2
Robert Haines <rhaines@manchester.ac.uk>
2022-01-04 10:50:48 +0000

    Update to Spring Boot version 2.5.8.
    This is the latest release in the 2.5 branch, and includes the latest
    security updates for log4j.
    
    Minor tweaks to the REST/HATEOAS configuration required, but otherwise a
    simple enough update.

--- src/main/java/hello/config/Hateoas.java
+++ src/main/java/hello/config/Hateoas.java
@@ -2,34 +2,23 @@ package hello.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.hateoas.server.LinkRelationProvider;
 import org.springframework.hateoas.server.core.EvoInflectorLinkRelationProvider;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class Hateoas {
+public class Hateoas implements RepositoryRestConfigurer {
 
 	private final static Logger log = LoggerFactory.getLogger(Hateoas.class);
 
-	@Bean
-	public LinkRelationProvider relProvider() {
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.setBasePath("/api");
+		log.info("Set REST API base path to '/api'");
+
+		config.setLinkRelationProvider(new EvoInflectorLinkRelationProvider());
 		log.info("Using EvoInflectorLinkRelationProvider for HATEOAS relations");
-
-		return new EvoInflectorLinkRelationProvider();
-	}
-
-	@Bean
-	public RepositoryRestConfigurer repositoryRestConfigurer() {
-
-		return new RepositoryRestConfigurer() {
-
-			@Override
-			public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-				config.setBasePath("/api");
-			}
-		};
 	}
 }
