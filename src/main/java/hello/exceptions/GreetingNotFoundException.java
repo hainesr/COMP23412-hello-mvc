b6633c7d2d5f078c7b249c28fc8842c9cd00eb38
Robert Haines <rhaines@manchester.ac.uk>
2022-01-04 15:09:08 +0000

    Add an `id` field to `GreetingNotFoundException`.
    This is so that we can easily get the `id` for error messages and so on.

--- src/main/java/hello/exceptions/GreetingNotFoundException.java
+++ src/main/java/hello/exceptions/GreetingNotFoundException.java
@@ -4,7 +4,15 @@ public class GreetingNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
+	private long id;
+
 	public GreetingNotFoundException(long id) {
 		super("Could not find greeting " + id);
+
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
 	}
 }
