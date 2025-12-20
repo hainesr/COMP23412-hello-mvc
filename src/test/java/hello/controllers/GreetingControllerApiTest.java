491ef30f254dc0641a92e6ba54503df35bb6dbc2
Robert Haines <rhaines@manchester.ac.uk>
2025-12-20 16:19:05 +0000

    Update to Spring Boot 3.4.

--- src/test/java/hello/controllers/GreetingControllerApiTest.java
+++ src/test/java/hello/controllers/GreetingControllerApiTest.java
@@ -27,9 +27,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -48,7 +48,7 @@ public class GreetingControllerApiTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@MockBean
+	@MockitoBean
 	private GreetingService greetingService;
 
 	@Test
--- src/test/java/hello/controllers/GreetingControllerTest.java
+++ src/test/java/hello/controllers/GreetingControllerTest.java
@@ -30,9 +30,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -50,7 +50,7 @@ public class GreetingControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
-	@MockBean
+	@MockitoBean
 	private GreetingService greetingService;
 
 	@Mock
