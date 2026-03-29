088fbe8080d949e0a13d843a41aebd509e04bbbc
Robert Haines <rhaines@manchester.ac.uk>
2026-03-29 15:41:13 +0100

    DELETE a greeting via the web.
    Claude prompts:
    ```
    implement code in GreetingController class to pass the new test.
    ```
    
    ```
    great. please move the new method to the end of the class
    ```

--- src/main/java/hello/controllers/GreetingController.java
+++ src/main/java/hello/controllers/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -80,6 +81,13 @@ public class GreetingController {
 
 		greetingService.save(greeting);
 		redirectAttrs.addFlashAttribute("ok_message", "New greeting added.");
+
+		return "redirect:/greetings";
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteGreeting(@PathVariable("id") long id) {
+		greetingService.deleteById(id);
 
 		return "redirect:/greetings";
 	}
